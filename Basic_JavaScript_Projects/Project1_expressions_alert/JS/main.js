8db3ec3673fd9b01ef40fd0c3879e4b9b64b7d7c
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-07 23:24:18 -0500

    Add comments to JavaScript practice code

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,21 +1,21 @@
-// alert("Hellooo, Nurse!!!");
-// window.alert("Rah, chah, Rahchahah");
-// document.write("Hello there, muthaf***!");
+// alert("Hellooo, Nurse!!!");                  // Create an alert box.
+// window.alert("Rah, chah, Rahchahah");        // Create an alert box, with window.alert.
+// document.write("Hello there, muthaf***!");   // Create a line of text on the page.
 
-// let A = "This is a string";
-// document.write(A);
+// let A = "This is a string";  // Create a variable, and give it a string value.
+// document.write(A);           // Create a line of text, using the string saved in variable 'A'.
 
-// let foo = "Foo Bar!";
-// window.alert(foo);
+// let foo = "Foo Bar!";    // Create a variable, and give it a string value.
+// window.alert(foo);       // Create a window alert box, using the string saved in variable 'foo'.
 
-// let quote = "Don't listen to the \"I know everything\" people. They're idiots!";
-// document.write(quote);
+// let quote = "Don't listen to the \"I know everything\" people. They're idiots!";     // Create a variable, and give it a string value.
+// document.write(quote);                                                               // Create a line of text on the page, using the 'quote' variable.
 
-// let quote = "\"Be who you are, and say what you feel, "
-// + "because those who mind don't matter, "
-// + "and those who matter don't mind\"";
+// let quote = "\"Be who you are, and say what you feel, "  // Create a multiline quote
+// + "because those who mind don't matter, "                // that spans multiple lines
+// + "and those who matter don't mind\"";                   // and concatenates then together into a single line of text
 
-// document.write(quote);
+// document.write(quote);                                   // Create a line of text on the page, using the 'quote' variable.
 
-let var1 = "this", var2 = "that", var3 = "this and that";
-document.write(var3);
+// let var1 = "this", var2 = "that", var3 = "this and that";    // Create multiple variables, and give them values, using a single 'let' (also 'var')
+// document.write(var3);                                        // Create a line of text on the page, using one of the variables from 'var1' to 'var3'.
