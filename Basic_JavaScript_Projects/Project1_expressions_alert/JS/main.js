f07a2a485185b6d296d7fd82b999da234d8b311a
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-07 23:12:51 -0500

    Add practice content using JavaScript

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,21 @@
 // alert("Hellooo, Nurse!!!");
 // window.alert("Rah, chah, Rahchahah");
-document.write("Hello there, muthaf***!");
+// document.write("Hello there, muthaf***!");
+
+// let A = "This is a string";
+// document.write(A);
+
+// let foo = "Foo Bar!";
+// window.alert(foo);
+
+// let quote = "Don't listen to the \"I know everything\" people. They're idiots!";
+// document.write(quote);
+
+// let quote = "\"Be who you are, and say what you feel, "
+// + "because those who mind don't matter, "
+// + "and those who matter don't mind\"";
+
+// document.write(quote);
+
+let var1 = "this", var2 = "that", var3 = "this and that";
+document.write(var3);
