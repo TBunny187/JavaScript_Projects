b71342202836962e400bef3239cf959fbab509bb
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-08 21:53:57 -0500

    Create main.js and utilize within index.html

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+let over = "You're hovering over the button!";
+let out = "You're not hovering over the button!";
+
+function func1() {
+    document.getElementById("text").innerHTML = over;
+};
+
+function func2() {
+    document.getElementById('text').innerHTML = out;
+};
