2ccb794713a42816c5f625de31a28a746a5b37ff
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-08 22:08:57 -0500

    Add comments for future reference

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,16 +1,19 @@
 let over = "You're hovering over the button!";
 let out = "You're not hovering over the button!";
 
+// Create function for onmouseover event
 function func1() {
     document.getElementById("text").innerHTML = over;
 };
 
+// Create function for onmouseout event
 function func2() {
     document.getElementById('text').innerHTML = out;
 };
 
+// Create function for concatenation
 function func3() {
-    let sentence = "I'm learning a lot";
-    sentence += " from this bootcamp!";
+    let sentence = "I'm learning a lot";    // create var to concat to
+    sentence += " from this bootcamp!";     //concat onto sentence variable
     document.getElementById('concatenate').innerHTML = sentence;
 }
