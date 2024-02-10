ca62a30dda9e79f0c60831e6aba119f2e56965a8
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 10:37:21 -0500

    Create functions for logical and/or true/false

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -62,20 +62,42 @@ function equalFalse(element) {
     element.innerHTML = 10 == 11;
 };
 
-// Display true from tripple equals
+// Display true from triple equals
 function sameAll(element) {
     element.innerHTML = 10 === 10;
 };
 
-// Display false from tripple equals
+// Display false from triple equals
 function diffAll(element) {
     element.innerHTML = 12 === "10";
 };
 
+// Display false from triple equals with same Value
 function sameVal(element) {
     element.innerHTML = 12 === "12";
 };
 
+// Display false from triple equals with same type
 function sameType(element) {
     element.innerHTML = "10" === "12";
+};
+
+// Display true from logical 'and'
+function andLogicTrue(element) {
+    element.innerHTML = 5 > 3 && 3 > 2;
+};
+
+// Display false from logical 'and'
+function andLogicFalse(element) {
+    element.innerHTML = 5 > 6 && 3 > 2;
+};
+
+// Display true from logical 'or'
+function orLogicTrue(element) {
+    element.innerHTML = 5 > 3 || 3 > 3;
+};
+
+// Display flase from logical 'or;
+function orLogicFalse(element) {
+    element.innerHTML = 2 > 3 || 3 > 3;
 };
