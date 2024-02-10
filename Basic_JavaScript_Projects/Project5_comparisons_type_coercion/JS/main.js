1e44a9c649f79776989e043b415fdf1ec3082d6a
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 09:39:13 -0500

    Create functions to display type coercion
    Create 2 functions, one that uses type coercion to change output of a string and a number to a longer string, and another to change output of a string and number, to a mathematical operation, display a number.

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,14 @@
-document.write(typeof 5);
+// Print 'typeof' number 5 to screen
+// document.write(typeof 5);
+
+// Use type coercion to create string of numbers
+function typeString(element) {
+    let x = "12" + 10;
+    element.innerHTML = '"12" + 10 = ' + x;
+};
+
+// Use type coercion to create subtraction operation
+function typeNum(element) {
+    let x = 12 - "10";
+    element.innerHTML = '12 - "10" = ' + x;
+};
