1a53bb8aac04f5e12414a6db6b629d23cb0377d1
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 09:24:48 -0500

    Create dictionary, function to display dict entry

--- Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -7,4 +7,17 @@ let dict = {
 
 function dictOutPut(element) {
     element.innerHTML = dict.Breed;
-}
+};
+
+let dict2 = {
+    "Animal": "Dog",
+    "Breed": "Jack Russell",
+    "Age": 3,
+    "Sound": "Bark!",
+};
+
+delete dict2.Breed;
+
+function dictDelete(element) {
+    element.innerHTML = dict2.Breed;
+};
