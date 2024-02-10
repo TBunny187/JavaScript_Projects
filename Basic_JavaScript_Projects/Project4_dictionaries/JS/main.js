a9d400a1dd5587692812d14d9cac80e745d3ce88
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 09:25:57 -0500

    Add comments

--- Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -1,3 +1,4 @@
+// Create a dictionary
 let dict = {
     "Animal": "Dog",
     "Breed": "Shiba Inu",
@@ -5,10 +6,12 @@ let dict = {
     "Sound": "Arf! Arf!",
 };
 
+// Display breed of animal
 function dictOutPut(element) {
     element.innerHTML = dict.Breed;
 };
 
+// Create another dictionary
 let dict2 = {
     "Animal": "Dog",
     "Breed": "Jack Russell",
@@ -16,8 +19,10 @@ let dict2 = {
     "Sound": "Bark!",
 };
 
+// Delete 'Breed' from dict2
 delete dict2.Breed;
 
+// Display 'undefined' from the deleted 'Breed' of dict2
 function dictDelete(element) {
     element.innerHTML = dict2.Breed;
 };
