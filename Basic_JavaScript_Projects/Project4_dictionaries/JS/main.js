864ba1cde57e33185844d080fb5056414b90daf6
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 09:17:31 -0500

    Create Project4_dictionaries file, skeleton html, main.js

---
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+let dict = {
+    "Animal": "Dog",
+    "Breed": "Shiba Inu",
+    "Age": 5,
+    "Sound": "Arf! Arf!",
+};
+
+function dictOutPut(element) {
+    element.innerHTML = dict.Breed;
+}
