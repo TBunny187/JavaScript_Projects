15080396704b955a4cbbbecb887e879cb9f6f402
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-11 11:56:14 -0500

    Create function to check hours, display greeting

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -1,14 +1,29 @@
-let glob = 'Global variable';
+// let glob = 'Global variable';
 
-function locVar() {
-    let loc = 'Local Variable';
-    console.log(loc);
-};
+// function locVar() {
+//     let loc = 'Local Variable';
+//     console.log(loc);
+// };
 
-function locVarErr() {
-    console.log(loc);
-};
+// function locVarErr() {
+//     console.log(loc);
+// };
 
-console.log(glob);
-locVar();
-locVarErr();
+// console.log(glob);
+// locVar();
+// locVarErr();
+
+// Get local time hour
+let dh = new Date().getHours();
+
+// Display greeting for time of day (morning, afternoon, evening)
+function timeCheck() {
+    let greet = document.getElementById('displayGreeting');
+    if (dh < 12) {
+        greet.innerHTML = 'Good Morning!';
+    } else if (dh < 1700) {
+        greet.innerHTML = 'Good Afternoon!';
+    } else {
+        greet.innerHTML = 'Good Evening!';
+    }
+}
