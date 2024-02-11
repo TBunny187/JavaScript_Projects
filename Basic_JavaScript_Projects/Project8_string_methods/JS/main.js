861527db60f1b2bff31d655fc75c066a7244503a
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-11 14:21:45 -0500

    Change concat function call to button

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -1,10 +1,11 @@
+// Create function to concatenate multiple strings into single sentence
 function concatStrs() {
+    let concatStr = document.getElementById('concatStr');
+
     let one = 'I concatenated ';
     let two = 'all of these parts ';
     let three = 'into a single long ';
     let four = 'sentence, using concat().';
-
-    document.write(one.concat(two, three, four));
-}
-
-concatStrs();
+    
+    concatStr.innerHTML = one.concat(two, three, four);
+};
