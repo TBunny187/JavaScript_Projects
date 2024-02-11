56860b7349ed3bc1d6af3a6c34d33eb82d28699e
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-02-10 23:56:23 -0500

    Create new Object and display info on page

--- Basic_JavaScript_Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -11,3 +11,20 @@ function checkAge() {
     let checkValue = age < 1 ? "Please enter your age!": age > 17 ? "You are old enough to vote!" : "You are not old enough to vote!";
     ageCheck.innerHTML = checkValue;
 };
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+};
+
+let Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+let Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+let Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+function objFunc() {
+    document.getElementById("New_and_This").innerHTML = 
+    'Emily drives a ' + Emily.Vehicle_Color + '-colored ' + Emily.Vehicle_Model +
+    ' manufactured in ' + Emily.Vehicle_Year;
+}
